0889911b0cfb61ab24481eb1f5e76cc07dfcf69e
Felipe_Reyes <felipe.reyes@dicipa.com.mx>
2019-09-15 00:13:20 -0500

    Working logout

--- apicalls/user.logout.js
+++ apicalls/user.logout.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import {Alert} from 'react-native';
 
-const userLogOut = (callback) => {
-    axios.post("tokenrequest/logout")
+const userLogOut = (token,callback) => {
+    axios.post("tokenrequest/logout",{
+        token
+    })
     .then(callback)
-    .catch((error)=>{
+    .catch(() => {
         Alert.alert("No fue posible cerrar su sesión");
     });
 }
--- screens/SideMenu.js
+++ screens/SideMenu.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { View , Button} from 'react-native';
+import {
+    View, Button, StyleSheet,
+    Alert
+} from 'react-native';
 import SideMenuItem from '../components/SideMenuItem'
 import { Navigation } from 'react-native-navigation'
 import { connect } from 'react-redux';
 import barCodeIcon from '../assets/iconbarcode.png'
-import {logOut} from '../apicalls/user.logout';
+import logOut from '../apicalls/user.logout';
+import Auth from './Auth';
 
 class SideMenu extends React.Component {
     optionClickHandle = (option, title) => {
@@ -43,43 +47,67 @@ class SideMenu extends React.Component {
         });
     }
 
-    logOut = () => {
-        logOut();
+    userLogOut = () => {
+        logOut(this.props.token, (response) => {
+            try {
+                Alert.alert("Queda pendiente llamar login");
+            } catch (reason) {
+                Alert("Error al cerrar sesión ");
+            }
+        });
     }
     render() {
         return (
             <View style={{ paddingTop: 50, height: "100%", backgroundColor: '#8c30f1' }} >
-                <SideMenuItem optionClickHandle={() => this.optionClickHandle("CyclicCountList", "Conteo Ciclico")}
-                    optionName="Conteo Ciclico" />
-                <SideMenuItem optionClickHandle={() => this.optionClickHandle("QueryArticles", "Consulta de Existensias")}
-                    optionName="Consulta de Existensias" />
-                <SideMenuItem optionClickHandle={() => this.optionClickHandle("PlaceSign", "Firma")}
-                    optionName="Firma Algo" />
-                <SideMenuItem optionClickHandle={() => this.optionClickHandle("ProductsPickup", "Recoleccion de Producto")}
-                    optionName="Recoleccion de Producto" />
-                <SideMenuItem
-                    optionName="Salidas por Caducidad"
-                    optionClickHandle={() =>
-                        this.optionClickHandle("InventoryExpire", "Salidas por Caducidad")}
-                />
-                <SideMenuItem optionName="Salidas por Transferencia"
-                    optionClickHandle={() =>
-                        this.optionClickHandle("InventoryTransfer", "Salidas por Transferencia")}
-                />
-                <SideMenuItem optionName="Orden de Venta"
-                    optionClickHandle={() =>
-                        this.optionClickHandle("SaleOrder", "Orden de Venta")}
-                />
-
-                <Button title="SALIR" onPress={logOut} />
+                <View style={{ height: '80%' }}>
+                    <SideMenuItem optionClickHandle={() => this.optionClickHandle("CyclicCountList", "Conteo Cíclico")}
+                        optionName="Conteo Ciclico" />
+                    <SideMenuItem optionClickHandle={() => this.optionClickHandle("QueryArticles", "Consulta de Existensias")}
+                        optionName="Consulta de Existensias" />
+                    <SideMenuItem optionClickHandle={() => this.optionClickHandle("PlaceSign", "Firma")}
+                        optionName="Firma Algo" />
+                    <SideMenuItem optionClickHandle={() => this.optionClickHandle("ProductsPickup", "Recolección de Producto")}
+                        optionName="Recoleccion de Producto" />
+                    <SideMenuItem
+                        optionName="Salidas por Caducidad"
+                        optionClickHandle={() =>
+                            this.optionClickHandle("InventoryExpire", "Salidas por Caducidad")}
+                    />
+                    <SideMenuItem optionName="Salidas por Transferencia"
+                        optionClickHandle={() =>
+                            this.optionClickHandle("InventoryTransfer", "Salidas por Transferencia")}
+                    />
+                    <SideMenuItem optionName="Orden de Venta"
+                        optionClickHandle={() =>
+                            this.optionClickHandle("SaleOrder", "Orden de Venta")}
+                    />
+                </View>
+                <View style={{ height: '20%' }}>
+                    <View style={styles.logout} >
+                        <View style={{ width: 200 }} >
+                            <Button title="SALIR" onPress={this.userLogOut} />
+                        </View>
+                    </View>
+                </View>
 
             </View>
         )
     }
 }
+const styles = StyleSheet.create({
+    logout: {
+        width: '100%',
+        height: 45,
+        padding: 5,
+        backgroundColor: 'transparent',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
 const mapStateToProps = state => {
     return {
         currentScreen: state.currentScreen,
+        token: state.user.token,
     }
 }
 export default connect(mapStateToProps)(SideMenu);
