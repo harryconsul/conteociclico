88bfc399a7f70d1bc3be37391c3563b84c60e284
Felipe_Reyes <felipe.reyes@dicipa.com.mx>
2020-05-08 14:19:21 -0500

    Recolección, mejora en la impresión

--- screens/ProductsPickup.js
+++ screens/ProductsPickup.js
@@ -188,29 +188,19 @@ class ProductsPickup extends React.Component {
                     rid: response.data.rid,
                 }
 
-                const printRows = new Map();
-                for (let i = 0; i < rawRows.length; i++) {
-
-                    if (parseInt(rawRows[i].sLastStat_38.value) == 560) {
-                        const key = rawRows[i].rowIndex;
-                        const value = {
-                            key,
-                            rowId: rawRows[i].rowIndex,
-                        }
-
-                        printRows.set(key, value);
-                    }
-                }
-
-                const list = [];
-                for (let article of printRows.values()) {
-                    list.push({ ...article });
-                }
+                const lineas = rawRows.map(row => ({
+                    status: row.sLastStat_38.value,
+                    rowId: "1." + String(row.rowIndex),
+                }));
+                
+                const list = lineas.filter((item)=> item.status === "560");
 
                 if (list.length > 0) {
                     printShipment(this.props.token, stack, list, (response) => {
                         resolve(true);
                     })
+                }else{
+                    Alert.alert("No existen lineas en estatus 560 para Imprimir");
                 }
             }, (reason) => reject(reason));
         });
